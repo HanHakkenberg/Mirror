3784b1084ec90b3bcb2dd369f1f6a1dce495d7e6
han hakkenberg <han.hakkenberg@gmail.com>
2019-01-07 17:04:32 +0100

    Movement

--- Mirror/Assets/Scripts/Movement.cs
+++ Mirror/Assets/Scripts/Movement.cs
@@ -3,9 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
-    Rigidbody myRB;
+    [SerializeField] float gravity = 9.81f;
+    private Rigidbody myRB;
+    [SerializeField] float moveSpeed;
 
-    void Awake() {
+    private void Awake() {
         myRB = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        UpdateMovement();
+    }
+
+    float newMovPosition;
+    private void UpdateMovement() {
+        newMovPosition = transform.position.x + Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed;
+
+        myRB.MovePosition(new Vector3(newMovPosition, transform.position.y));
+    }
+
+    private void FixedUpdate() {
+        Gravity();
+    }
+
+    private void Gravity() {
+        myRB.AddForce(new Vector3(0, gravity, 0));
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Damage")) {
+            Debug.Log("Damage");
+        }
+        else {
+            Debug.Log("other");
+        }
     }
 }
